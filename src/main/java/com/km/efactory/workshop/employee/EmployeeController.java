63394bd54d0a3792505e49e4b0a4a1a3217c5fa9
krzysiek <you@example.com>
2023-03-30 17:22:05 +0200

    employeeService updates

--- src/main/java/com/km/efactory/workshop/employee/EmployeeController.java
+++ src/main/java/com/km/efactory/workshop/employee/EmployeeController.java
@@ -1,16 +1,23 @@
 package com.km.efactory.workshop.employee;
 
 import java.lang.reflect.Field;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Stream;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.km.efactory.workshop.employee.exceptions.EmployeeIllegalStateException;
 
+import jakarta.websocket.server.PathParam;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -18,39 +25,42 @@ import lombok.RequiredArgsConstructor;
 public class EmployeeController {
     private EmployeeService employeeService;
 
+    @GetMapping(value = "/employees")
     public  ResponseEntity<List<Employee>> getAllEmployees() {
         List<Employee> allEmployees = this.employeeService.getAllEmployees();
         return new ResponseEntity<>(allEmployees,HttpStatus.OK);
     }
 
-    public ResponseEntity<Employee> getEmployeeById(Long id) {
+    @GetMapping(value = "/employees/{id}")
+    public ResponseEntity<Employee> getEmployeeById(@PathParam("id") Long id) {
         Employee employee = this.employeeService.getEmployeeById(id)
             .orElseThrow(() -> new EmployeeIllegalStateException("Employee by Id:"+id+" not found."));
         return new ResponseEntity<>(employee,HttpStatus.OK);
     }
 
+    @PostMapping(value = "/employees")
     public ResponseEntity<?> createEmployee(@RequestBody Employee employee) {
-        //dopisac sprawdzenie empployee
-        Field[] emlployeeFields = employee.getClass().getFields();
-        Stream<Field> streamFields =  Stream.of(emlployeeFields).filter((field)-> field.getName().equals("id") );
-        boolean employeeFiledsValueNotNull = streamFields.allMatch((field) -> {
-            try {
-                return !field.get(field.getName()).equals(null);
-            } catch (IllegalArgumentException e) {
-                //e.printStackTrace();
-                return false;
-            } catch (IllegalAccessException e) {
-                //e.printStackTrace();
-                return false;
-            }
-        } );
-        return  employeeFiledsValueNotNull ? new ResponseEntity<Employee>(employee,HttpStatus.OK) 
-                : new ResponseEntity<String>("Employees properties illegal value.",HttpStatus.BAD_REQUEST) ;
+        Employee createdEmployee = null;
+        try {
+             createdEmployee = this.employeeService.createEmployee(employee);
+        } catch (EmployeeIllegalStateException e) {
+            throw new EmployeeIllegalStateException("Employee attributes: "+e.getMessage()+" have invalid/null values.");
+        }
+        return new ResponseEntity<>(createdEmployee,HttpStatus.CREATED);    
     }
 
-    public ResponseEntity<String> deleteEmployeeById(Long id) {
+    @PutMapping(value = "/employees/{id}")
+    public ResponseEntity<Employee> updateEmployee(@PathParam("id") Long id,@RequestBody Employee employee) {
+
+        return new ResponseEntity<>(null,HttpStatus.ACCEPTED);
+    }
+
+    @DeleteMapping("/employees/{id}")
+    public ResponseEntity<String> deleteEmployeeById(@PathParam("id") Long id) {
         this.employeeService.deleteEmployeeById(id);
         String msg = "Employee by Id: "+ id + "deleted.";
         return new ResponseEntity<>(msg,HttpStatus.OK);
     }
+
+   
 }
