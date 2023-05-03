864e710c708d48861626c860a02562470704e28b
krzysiek <k@m.pl>
2023-05-03 02:12:25 +0200

    security updates

---
+++ src/main/java/com/km/efactory/workshop/security/configuration/WebSecurityConfig.java
@@ -0,0 +1,84 @@
+package com.km.efactory.workshop.security.configuration;
+
+import com.km.efactory.workshop.employee.EmployeeRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
+
+import com.km.efactory.workshop.security.jwt.JwtAuthenticationFilter;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class WebSecurityConfig {
+
+    private final AuthenticationProvider authenticationProvider;
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+    private final LogoutHandler logoutHandler;
+    private final EmployeeRepository employeeRepository;
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return companyId -> this.employeeRepository.findByCompanyId(companyId)
+                .orElseThrow(() -> new UsernameNotFoundException("Employee not found."));
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService());
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
+
+    }
+
+    @Bean
+    AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+
+        return new BCryptPasswordEncoder();
+    }
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    return http
+        .csrf(csrf -> csrf.disable())
+        .authorizeHttpRequests(authorizeRequest ->
+            authorizeRequest
+                .requestMatchers("/api/v1/auth/logout","/api/v1/auth/authenticated").permitAll()
+                .anyRequest().authenticated()
+        )
+        .sessionManagement(sessionManagement ->
+            sessionManagement
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        )
+        .authenticationProvider(authenticationProvider)
+        .addFilterBefore(jwtAuthenticationFilter,UsernamePasswordAuthenticationFilter.class)
+        .logout(logout ->
+            logout
+                .logoutUrl("/api/v1/logout")
+                .addLogoutHandler(logoutHandler)
+                .logoutSuccessHandler((request,response,authentication) -> SecurityContextHolder.clearContext())
+        )
+        .build();
+    }
+}
